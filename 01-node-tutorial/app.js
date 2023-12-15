930008e8239e464627e4bfc6a03c31a8ffe2e652
Jonathon Lee <jonnyleealas@gmail.com>
2023-12-14 23:40:07 -0600

    res.end

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,7 @@
-console.log('Welcome to Node Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    console.log('server hit')
+    res.end('whats up doc')
+})
+server.listen(5000)
