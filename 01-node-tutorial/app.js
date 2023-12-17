c2cfe5fbc19c06413787242e554a1cc812169e31
jonathon lee <jonnyleealas@gmail.com>
2023-12-16 19:26:03 -0600

    Update app.js

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,7 +1,28 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-    console.log('server hit')
-    res.end('whats up doc')
-})
+    const url = req.url
+if(url === '/hanni'){
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>hanni hanni hanni hanni</h1>')
+    res.end()
+}
+if(url === '/'){
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>home page</h1>')
+    res.end()
+}
+
+if(url === '/minji'){
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>minji minji</h1>')
+    res.end()
+}
+   
+else{
+    res.writeHead(404, {'content-type': 'text/html'})
+    res.write('<h1>Page not found</h1>')
+    res.end()
+}
+}) 
 server.listen(5000)
