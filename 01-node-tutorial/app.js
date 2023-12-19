7c8e3ab52ef1830b6bcc97a36668c287a4e3eabe
Jonathon Lee <jonnyleealas@gmail.com>
2023-12-18 19:19:22 -0600

    res.send()

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,28 +1,17 @@
-const http = require('http')
+const express = require('express');
+const app = express()
 
-const server = http.createServer((req, res)=>{
-    const url = req.url
-if(url === '/hanni'){
-    res.writeHead(200, {'content-type': 'text/html'})
-    res.write('<h1>hanni hanni hanni hanni</h1>')
-    res.end()
-}
-if(url === '/'){
-    res.writeHead(200, {'content-type': 'text/html'})
-    res.write('<h1>home page</h1>')
-    res.end()
-}
+app.get('/', (req, res) => {
+    res.send('Home  bittttchhhesss')
+})
 
-if(url === '/minji'){
-    res.writeHead(200, {'content-type': 'text/html'})
-    res.write('<h1>minji minji</h1>')
-    res.end()
-}
-   
-else{
-    res.writeHead(404, {'content-type': 'text/html'})
-    res.write('<h1>Page not found</h1>')
-    res.end()
-}
-}) 
-server.listen(5000)
+app.listen(5000, () => {
+    console.log('listening on 5000')
+})
+
+// app.get
+// app.post
+// app.delete
+// app.all
+// app.use
+// app.listen
