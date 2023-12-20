67e14ff803576c596ce7627e1a7903f158bb71cc
Jonathon Lee <jonnyleealas@gmail.com>
2023-12-19 20:20:14 -0600

    req res status 404

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -2,29 +2,16 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).send('home page')
+    res.status(200).send('<h1>jisoo home</h1>')
 })
 
-app.get('/haerin', (req, res) => {
-
+app.get('/minji', (req, res) => {
+    res.status(200).send('<h1>minji minji</h1>')
 })
 
-app.get('/hyein', (req, res) => {
-
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>PAGE NOT FOUND</h1>')
 })
-
-app.get('/danielle', (req, res) => {
-
-})
-
-app.get('/lisa', (req, res) => {
-
-})
-
-app.all('*', (req, res) => {
-    res.status(400).send('resource not found')
-})
-
 app.listen(5000, () => {
     console.log('listening on 5000')
 })
