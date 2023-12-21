fd2c835991a03a5244f3b718523e8d62e639c572
Jonathon Lee <jonnyleealas@gmail.com>
2023-12-20 23:23:11 -0600

    json object get request

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const app = express()
+const {people, users} = require('./fakeData')
 
 app.get('/', (req, res) => {
-    res.status(200).send('<h1>jisoo home</h1>')
+    res.json(people)
 })
 
 app.get('/minji', (req, res) => {
-    res.status(200).send('<h1>minji minji</h1>')
+    res.json(users)
 })
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>PAGE NOT FOUND</h1>')
+app.all("*", (req, res) => {
+    res.send('<h1>PAGE NOT FOUND</h1>')
 })
+
 app.listen(5000, () => {
     console.log('listening on 5000')
 })
