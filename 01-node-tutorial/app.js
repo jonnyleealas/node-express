f3a7d70c3916a6ad0c02ca9eebcc3d7fb6a0aa4b
Jonathon Lee <jonnyleealas@gmail.com>
2023-12-31 14:23:21 -0700

    update app.js

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,23 +1,14 @@
 const express = require('express')
 const app = express()
-const {people} = require('./fakeData')
-const diaper = people
+const {people} = require('./fakedata')
 
-app.get('/homepage', (req, res) => {
-    res.status(200).send("<h1>Hi Bryan t(._.)t</h1>")
+app.get('/este', (req, res) => {
+    res.send('<h1>hello you fuckers</h1>')
 })
 
-app.get('/secretroute', (req, res) => {
-    res.send('<h1>secret route</h1>')
+app.get('/fakedata', (req, res) => {
+    res.send(people)
 })
-
-app.get('/fakeapi', (req, res) => {
-    res.status(200).send(diaper)
-})
-app.get('*', (req, res) => {
-    res.send('<h1>Page not found</h1>')
-})
-
-app.listen(9000, () => {
+app.listen(5000, () => {
     console.log('listening on 5000')
 })
