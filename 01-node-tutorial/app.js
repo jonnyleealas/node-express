258d35727945ac391c852c6b2f24f0769ec69d71
Jonathon Lee <jonnyleealas@gmail.com>
2024-01-02 20:49:38 -0600

    added dynamic routes params in url

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -6,8 +6,11 @@ app.get('/este', (req, res) => {
     res.send('<h1>hello you fuckers</h1>')
 })
 
-app.get('/fakedata', (req, res) => {
-    res.send(people)
+app.get('/:personId', (req, res) => {
+
+    const {personId} = req.params
+    const singlePerson = people.find((person) => person.title === String(personId))
+    res.json(singlePerson)
 })
 app.listen(5000, () => {
     console.log('listening on 5000')
--- 01-node-tutorial/fakeData.js
+++ 01-node-tutorial/fakeData.js
@@ -20,7 +20,7 @@ const people = [
     {
       "userId": 1,
       "id": 4,
-      "title": "eum et est occaecati",
+      "title": "jonny",
       "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
     },
     {
