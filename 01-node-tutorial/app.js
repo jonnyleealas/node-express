1fe37165e7d7e827adb9d4fb6928beded6c0665d
Jonathon Lee <jonnyleealas@gmail.com>
2024-01-04 16:32:16 -0600

    Update app.js

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,18 +1,41 @@
 const express = require('express')
 const app = express()
-const {people} = require('./fakedata')
+const { people } = require('./fakedata')
 
 app.get('/este', (req, res) => {
     res.send('<h1>hello you fuckers</h1>')
 })
 // params can be added in url using req.params
-app.get('/:personID', (req, res) => {
-    const {personID} = req.params
-    const singlePerson = people.find((person) => person.title === String(personID))
-    if(!singlePerson){
-        return res.status(404).send('Product does not exsist')
+app.get('/newjeans/:personID', (req, res) => {
+    const { personID } = req.params
+
+    const singlePerson = people.find((person) => person.id === Number(personID))
+
+    const singlePersonString = people.find((person) => person.title === String(personID))
+
+    if (!singlePerson) {
+        return res.status(404).send('person does not exsist')
     }
-    res.json(singlePerson)
+    
+
+        return res.json(singlePerson)
+    
+})
+
+app.get('/newjean/:personTitle', (req, res) => {
+    const {personTitle} = req.params
+
+    const singlePerson = people.find((person) => person.title === String(personTitle))
+
+    if(!singlePerson){
+        return res.status(404).send('Title not found')
+    }
+
+    return res.json(singlePerson)
+})
+
+app.get("*", (req, res) => {
+    res.status(404).send("<h1>Page not found</h1>")
 })
 
 app.listen(5000, () => {
