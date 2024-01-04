8f6027d40b881031f33a2dab4e1dea693a7d91b1
Jonathon Lee <jonnyleealas@gmail.com>
2024-01-03 19:21:04 -0600

    personID req.params must match

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -6,12 +6,15 @@ app.get('/este', (req, res) => {
     res.send('<h1>hello you fuckers</h1>')
 })
 // params can be added in url using req.params
-app.get('/:personId', (req, res) => {
-
-    const {personId} = req.params
-    const singlePerson = people.find((person) => person.title === String(personId))
+app.get('/:personID', (req, res) => {
+    const {personID} = req.params
+    const singlePerson = people.find((person) => person.title === String(personID))
+    if(!singlePerson){
+        return res.status(404).send('Product does not exsist')
+    }
     res.json(singlePerson)
 })
+
 app.listen(5000, () => {
     console.log('listening on 5000')
 })
