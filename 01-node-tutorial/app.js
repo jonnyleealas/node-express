d4aff342d6a878bdb9c9d4c96637865f468481fa
Jonathon Lee <jonnyleealas@gmail.com>
2024-01-25 08:16:30 -0600

    middleware

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,63 +1,23 @@
 const express = require('express')
 const app = express()
-const { people } = require('./fakedata')
 
-app.get('/este', (req, res) => {
-    res.send('<h1>hello you fuckers</h1>')
-})
-// params can be added in url using req.params
-app.get('/newjeans/:personID', (req, res) => {
-    console.log(req.params)
-    const { personID } = req.params
-    const singlePerson = people.find((person) => person.id === Number(personID))
-    const singlePersonString = people.find((person) => person.title === String(personID))
-    if (!singlePerson) {
-        return res.status(404).send('person does not exsist')
-    }
-        return res.json(singlePerson)
+app.get('/', (req, res) => {
+    const method = req.method
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(`Method: ${method}, Url: ${url}, time: ${time}`)
+    res.send('Home')
 })
 
-app.get('/newjean/:personTitle', (req, res) => {
-    const {personTitle} = req.params
-    const singlePerson = people.find((person) => person.title === String(personTitle))
-    if(!singlePerson){
-        return res.status(404).send('Title not found')
-    }
-    return res.json(singlePerson)
+app.get('/about', (req, res) => {
+    const method = req.method
+    const time = new Date().getFullYear()
+    const url = req.url
+    console.log(`Method: ${method}, Url: ${url}, time: ${time}`)
+    res.send("About")
 })
 
 
-app.get('/api/people', (req, res) => {
-    const newPeople = people.map((person) => {
-        const {id, title, body} = person
-        return {id, title, body}
-    })
-    res.json(newPeople)
-})
-
-app.get('/api/v1/query', (req, res) => {
-    const {search, limit} = req.query
-    let sortedPeople = [...people]
-    
-    if(search){
-        sortedPeople = sortedPeople.filter((people) => {
-            return people.title.startsWith(search)
-        })
-    }
-
-    if(limit){
-        sortedPeople = sortedPeople.slice(0, Number(limit))
-    }
-
-    res.status(200).json(sortedPeople)
-})
-
-
-app.get("*", (req, res) => {
-    res.status(404).send("<h1>Page not found</h1>")
-})
-
-
-app.listen(5000, () => { 
+app.listen(5000, () => {
     console.log('listening on 5000')
 })
