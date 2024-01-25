3f0964e246e282202243c6d93b3b4f3129925711
Jonathon Lee <jonnyleealas@gmail.com>
2024-01-25 08:24:20 -0600

    Update app.js

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-
+const logger = (req, res, next) => {
     const method = req.method
     const url = req.url
     const time = new Date().getFullYear()
-    console.log(`Method: ${method}, Url: ${url}, time: ${time}`)
+    console.log(method, url, time)
+    next()
+}
+
+app.get('/', logger, (req, res) => {
     res.send('Home')
 
 })
 
-app.get('/about', (req, res) => {
-    const method = req.method
-    const time = new Date().getFullYear()
-    const url = req.url
-    console.log(`Method: ${method}, Url: ${url}, time: ${time}`)
+app.get('/about', logger, (req, res) => {
     res.send("About")
 })
 
