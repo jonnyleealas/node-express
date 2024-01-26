4dd93a44e56e22b2d4754f3eef5b0f396ce582f2
Jonathon Lee <jonnyleealas@gmail.com>
2024-01-26 08:13:41 -0600

    using logger and app.use
    This will add middlewear to all routes after the using the funtion

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,23 +1,20 @@
 const express = require('express')
 const app = express()
+const logger = require('./extraFiles/logger')
 
-const logger = (req, res, next) => {
-    const method = req.method
-    const url = req.url
-    const time = new Date().getFullYear()
-    console.log(method, url, time)
-    next()
-}
+app.use(logger)
 
-app.get('/', logger, (req, res) => {
-    res.send('Home')
-
+app.get('/', (req, res) => {
+    res.send("home")
 })
 
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
     res.send("About")
 })
 
+app.get('*', (req, res) => {
+    res.send('page does not exsist')
+})
 
 app.listen(5000, () => {
 
