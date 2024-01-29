3c6fca209318b0ab3ef58f911c7fb19bde5a6ae1
Jonathon Lee <jonnyleealas@gmail.com>
2024-01-29 08:09:50 -0600

    using morgan middleware

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const authorize = require('./extraFiles/auth')
 const logger = require('./extraFiles/logger')
-app.use([logger, authorize])
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
 
 app.get("/", (req, res) => {
     res.send('home')
+})
+
+app.get('/api/items', (req, res) =>{
+    res.send('api/items')
 })
 
  app.listen('5000', () => {
