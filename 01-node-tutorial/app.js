1b8cdee4c629ad5c65b86f2f4053c764644a7840
Jonathon Lee <jonnyleealas@gmail.com>
2024-01-30 07:26:08 -0600

    post method

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -7,8 +7,15 @@ let {people} = require('./fakeData')
 
 app.use(express.static('./methods-public'))
 
+app.use(express.urlencoded({extended: false}))
+
 app.get('/api/people', (req, res) => {
     res.status(200).json({success: true, data: {people}})
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    res.send('POST METHOD WORKED')
 })
 
 app.listen('5000', () => {
