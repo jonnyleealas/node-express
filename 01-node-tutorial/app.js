3b9acffd23dcdc7ba4aadbf09f75a29d094ff2d8
Jonathon Lee <jonnyleealas@gmail.com>
2024-01-29 19:36:27 -0600

    GET method

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,19 +1,16 @@
 const express = require('express')
 const app = express()
-const authorize = require('./extraFiles/auth')
-const logger = require('./extraFiles/logger')
 const morgan = require('morgan')
+let {people} = require('./fakeData')
+
+
 
 app.use(morgan('tiny'))
 
-app.get("/", (req, res) => {
-    res.send('home')
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: {people}})
 })
 
-app.get('/api/items', (req, res) =>{
-    res.send('api/items')
-})
-
- app.listen('5000', () => {
+app.listen('5000', () => {
     console.log('listening on 5000')
 })
