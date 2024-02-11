da373ec48a6070f781160203ab423544609bff42
Jonathon Lee <jonnyleealas@gmail.com>
2024-02-11 17:13:57 -0600

    saved name

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,27 +1,52 @@
+// const express = require('express')
+// const app = express()
+// const {people} = require('./fakeData')
+
+// app.use(express.static('./methods-public'))
+// app.use(express.urlencoded({extended: false}))
+
+// app.get('/', (req, res) => {
+//     res.send('home')
+// })
+
+// app.get('/people', (req, res) => {
+//     res.status(200).json({success: true, data: people})
+// }) 
+
+// app.post('/login', (req, res) => {
+//     const {name} = req.body
+    
+//     if(name === jonny){
+//         return res.status(200).send(`welcome ${name} you are now logged in`)
+//     } else {
+
+//         res.send('no name provided')
+//     }
+// })
+
+// app.listen('5000', () => {
+//     console.log('listening on 5000')
+// })
+
 const express = require('express')
 const app = express()
-const {people} = require('./fakeData')
 
 app.use(express.static('./methods-public'))
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.send('home')
+app.get('/' ,(req, res) => {
+    res.send('this is the home page')
 })
 
-app.get('/people', (req, res) => {
-    res.status(200).json({success: true, data: people})
-}) 
-
 app.post('/login', (req, res) => {
-    const {name} = req.body
-    
-    if(name === jonny){
-        return res.status(200).send(`welcome ${name} you are now logged in`)
-    } else {
+  console.log(req.body)
+  const {name} = req.body
 
-        res.send('no name provided')
-    }
+  if(name === 'jonny'){
+    res.status(200).send('Post is working')
+  } else {
+    res.send('no name found')
+  }
 })
 
 app.listen('5000', () => {
