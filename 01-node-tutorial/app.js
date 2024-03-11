1786764312745fa5f36a61037dc922d36a5aad54
Jonathon Lee <jonnyleealas@gmail.com>
2024-03-10 22:55:11 -0500

    errors and form input data

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -6,13 +6,23 @@ let { people } = require("./fakeData")
 app.use(express.static('./methods-public'))
 // parses form and allows us to get data so we can use it: req.body
 app.use(express.urlencoded({extended: false}))
-
+// parse json data
+app.use(express.json())
 app.get('/', (req, res) => {
     res.status(200).send("home page")
 })
 
 app.get('/api/people', (req, res) => {
      res.status(200).json({success: true, data: people})
+})
+
+app.post('/api/people', (req, res) => {
+    const { name } = req.body
+
+    if(!name){
+        return res.status(400).json({success: false, msg:'fuck you add the correct shit'})
+    }
+    res.status(202).json({success:true, person:name})
 })
 
 app.post('/login', (req, res) => {
