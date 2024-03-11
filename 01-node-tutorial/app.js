bcdf294a92b63fef0517d823e61567669f1e6ead
Jonathon Lee <jonnyleealas@gmail.com>
2024-03-10 22:05:17 -0500

    Update app.js

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -2,16 +2,34 @@ const express = require('express')
 const app = express()
 let { people } = require("./fakeData")
 
+//stats html
 app.use(express.static('./methods-public'))
-
+// parses form and allows us to get data so we can use it: req.body
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.send("home page")
+    res.status(200).send("home page")
+})
+
+app.get('/api/people', (req, res) => {
+     res.status(200).json({success: true, data: people})
 })
 
 app.post('/login', (req, res) => {
+    const {name} = req.body
+    console.log('req.body:',req.body)
    
-    res.send("POST method successful")
+    if(name === ''){
+        res.status(404).send("please enter a password")
+    }
+
+    if(name !== "taco-app"){
+        res.status(401).send("incorrect password")
+    }
+
+    if(name === "taco-app"){
+        res.send(`Welcome to the website ${name}`)
+    } 
 })
 
 app.listen(5000, () => {
