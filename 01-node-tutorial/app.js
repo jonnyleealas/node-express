d6d7b692402dd511e653b5cd49b91367e7ff1262
Jonathon Lee <jonnyleealas@gmail.com>
2024-03-12 18:45:11 -0500

    Update app.js

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -56,8 +56,39 @@ app.post('/api/postman/people', (req,res) => {
 app.put('/api/postman/people/:id', (req, res) => {
     const {id} = req.params
     const {name} = req.body
-    console.log(id, name)
-    res.send('hello world')
+    
+    const person = people.find((person) => person.id === Number(id))
+
+    if(!person){
+        return res  
+            .status(404)
+            .json({success: false, msg: `person with id ${id} does not exsist`})
+    }
+
+    const newPeople = people.map((person) => {
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success: true, data: newPeople})
+    
+})
+
+app.delete('/api/postman/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+
+    const person = people.find((person) => person.id === Number(id))
+
+    if(!person){
+        return res
+            .status(404)
+            .json({success: false, msg:"user delete"})
+    }
+
+    const newPeople = people.filter((person) => person.id !== Number(id))
+    return res.status(200).json({success: true, data: newPeople})
 })
 
 app.listen(5000, () => {
