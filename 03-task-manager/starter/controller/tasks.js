0cb642e828e17832dc18b8e30a6b6615564d5242
Jonathon Lee <jonnyleealas@gmail.com>
2024-03-20 09:07:20 -0500

    data objects in front and back end differences

--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -2,26 +2,27 @@ const Task = require('../models/task')
 
 const getAllTasks = async (req, res) => {
     try{
-        const task = await Task.find({})
-        res.status(201).json({task})
+        const tasks = await Task.find({})
+        // res.status(200).json({task, amount:tasks.length})
+        res.status(200).json({succuss:true, data:{tasks}})
     } catch (err) {
-        res.status(500).json({msg:error})
+        res.status(500).json({msg:err})
     }
 }
 
 const createTask = async (req, res) => {
     try {
         const tasks = await Task.create(req.body)
-        res.status(200).json({tasks})
+        res.status(201).json({tasks})
     } catch (error) {
-        res.status(500).json({error})
+        res.status(500).json({msg:error})
     }
 }
 
 const getTask = async (req, res) => {
     try {
         const {id:taskID} = req.params
-        const task = await Task.findOneAndUpdate({_id:taskID})
+        const task = await Task.findOne({_id:taskID})
          
          if(!task){
             return res.status(404).json({msg: `no task with ID:${taskID}`})
