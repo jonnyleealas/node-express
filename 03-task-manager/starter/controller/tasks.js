dd9f5b61ae165e3f59332b04cbb5329be1ac6d9b
Jonathon Lee <jonnyleealas@gmail.com>
2024-03-22 08:49:32 -0500

    objects should match front and back end

--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -4,7 +4,7 @@ const getAllTasks = async (req, res) => {
     try{
         const tasks = await Task.find({})
         // res.status(200).json({task, amount:tasks.length})
-        res.status(200).json({data:{tasks}})
+        res.status(200).json({tasks})
     } catch (err) {
         res.status(500).json({msg:err})
     }
