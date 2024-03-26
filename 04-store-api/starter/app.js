b7593d7bb112e432fa0be8f702f3f17104ee59b6
Jonathon Lee <jonnyleealas@gmail.com>
2024-03-26 08:48:44 -0500

    new file connectDB

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,32 @@
-console.log('04 Store API')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// async errors
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+// middleware
+app.use(express.json())
+
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store Api</h1><a href="/api/v1/products">products route</a>')
+})
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const start = async () => {
+    try {
+        // ConnectDB
+        app.listen('3000')
+    } catch(error) {
+        res.send(error)
+    }
+}
