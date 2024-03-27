b8ed5a427b07b25b7ad474382e222f8462708ed8
Jonathon Lee <jonnyleealas@gmail.com>
2024-03-26 23:42:24 -0500

    added routes and db
    connect mongodb through server
    Give it dotenv var
    
    Create routes by creating controller get functions
    
    import them in routes
    
    Import routes into appjs

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
@@ -19,6 +21,8 @@ app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products"> Products Route</a>')
 })
 
+app.use('/api/v1/products', productsRouter)
+
 // products route
 
 app.use(notFoundMiddleware)
@@ -29,6 +33,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
     try {
         // connectDB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`server is listening on port ${port}`))
     } catch (error) {
      console.log(error)  
