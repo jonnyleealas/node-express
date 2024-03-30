731feb6e56ca2a8e0b59d4fce7873d5a721c9376
Jonathon Lee <jonnyleealas@gmail.com>
2024-03-30 18:32:10 -0500

    connect db with products array and add to mongo

--- 04-store-api/starter/models/product.js
+++ 04-store-api/starter/models/product.js
@@ -2,23 +2,32 @@ const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: [true, 'product name must be provided'],   
+        type:String,
+        required: [true, 'product name is required'],
     },
     price: {
         type: Number,
-        required:[true, 'product price must be provided'],
+        required: [true, "product price required"],
     },
-    featured: {
+    feartured: {
         type: Boolean,
-        default:false,
+        default: false,
     },
-    rating:{
+    rating: {
         type: Number,
         default: 4.5,
     },
-    created: {
+    createdAt: {
         type: Date,
         default: Date.now(),
+    },
+    company: {
+        type: String,
+        enum: {
+            values: ['ikea', 'liddy', 'caressa', 'marcos'],
+            message: '{VALUE} is not supported',
+        }
     }
 })
+
+module.exports = mongoose.model('product', productSchema)
