97d75f80dc0de13a457d3af9ec9e5415917d4443
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-03 22:07:46 -0500

    Update products.js

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -6,14 +6,24 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured} = req.query
+    const {featured, company, name, price} = req.query
     const queryObject = {}
     console.log(queryObject, 'hello')
     if(featured){
         queryObject.featured = featured === 'true' ? true : false
     }
+    if(company){
+        queryObject.company = company
+    }
+    if(name) {
+        queryObject.name = name
+    }
+    if(price){
+        queryObject.price = price
+    }
     console.log(queryObject)
     const products = await Product.find(queryObject)
+    console.log(products)
     res.status(200).json({products, nbHits: products.length})
 }
 
