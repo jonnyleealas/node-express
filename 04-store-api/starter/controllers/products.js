3ea39736d185b44efbe27331570b45a29885e57e
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-05 00:55:30 -0500

    added filtering with regex mongodb

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,25 +1,31 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({featured: true})
+    const search = 'ik'
+    const products = await Product.find({ 
+        company: { $regex: search, $options: 'i' }
+    })
     res.status(200).json({ products, nbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured, company, name, price} = req.query
+    const {featured, company, name, price, rating} = req.query
     const queryObject = {}
     console.log(queryObject, 'hello')
     if(featured){
         queryObject.featured = featured === 'true' ? true : false
     }
     if(company){
-        queryObject.company = company
+        queryObject.company = {$regex: company, $options: 'i'}
     }
     if(name) {
-        queryObject.name = name
+        queryObject.name = {$regex: name, $options: 'i'}
     }
     if(price){
-        queryObject.price = price
+        queryObject.price = {$regex: price, $options: 'i'}
+    }
+    if(rating){
+        queryObject.rating = {$regex: rating, $options: 'i'}
     }
     console.log(queryObject)
     const products = await Product.find(queryObject)
