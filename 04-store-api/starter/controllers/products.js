7ef3c4eb7aa654a63b978a4b40669efc03f850dd
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-07 15:03:14 -0500

    Update products.js

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,11 +2,12 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
     const search = 'ik'
-    const products = await Product.find({}).sort('name price')
+    const products = await Product.find({}).select('name price').limit(4)
     res.status(200).json({ products, nbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
+    console.log(req.query, 'query')
     const {featured, company, name, price, rating, sort, field} = req.query
     const queryObject = {}
     console.log(queryObject, 'hello')
@@ -38,6 +39,12 @@ const getAllProducts = async (req, res) => {
         const fieldList = field.split(',').join(' ')
         result = result.select(fieldList)
     }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
 
     const products = await result
     res.status(200).json({products, nbHits: products.length})
