f928dfeedef97228566ac5d6f9075e150722fa9d
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-08 21:42:30 -0500

    jwt /api/v1/

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,16 @@
+
+
+const login = async (req, res) => {
+    res.send('fake login/register/signup route')
+}
+
+const dashboard = async ( req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100)
+    res.status(200).json({msg:`hello, john doe`, secret: `here is your autherized data. Your lucky number is ${luckyNumber}`})
+}
+
+
+module.exports = {
+    login,
+    dashboard
+}
