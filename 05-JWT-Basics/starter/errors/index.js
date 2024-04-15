1c8a72b2bcd53f2daadcf411fb95c5ab44b8b392
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-14 20:11:14 -0500

    adding custom status codes
    use package http-status-codes to add to our error handlers

---
+++ 05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error')
+const BadRequest = require('./bad-request')
+const UnAuthenticated = require('./unauthenticated')
+
+module.exports = {
+    CustomAPIError,
+    BadRequest,
+    UnAuthenticated,
+}
