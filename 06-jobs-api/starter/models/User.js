844c4494aafdc4c007547522800dc5f21bcffd18
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-17 00:09:56 -0500

    schema for mongoose validator

---
+++ 06-jobs-api/starter/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        typs: String,
+        required: [true, 'Please provide name'],
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email'
+        ],
+        unique: true,
+    }
+})
