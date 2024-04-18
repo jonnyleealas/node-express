50f36f3a4d698e010f88a24453955ac90c63cf8c
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-17 23:49:37 -0500

    cleared typo

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -2,25 +2,25 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
     name: {
-        typs: String,
+        type: String,
         required: [true, 'Please provide name'],
-        minlength: 3,
         maxlength: 50,
+        minlength: 3,
     },
     email: {
         type: String,
         required: [true, 'Please provide email'],
         match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'Please provide a valid email'
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          'Please provide a valid email',
         ],
         unique: true,
-    },
+      },
     password: {
         type: String,
         required: [true, 'Please provide password'],
         minlength:3,
-        maxlength: 20,
+        maxlength: 50,
     }
 })
 
