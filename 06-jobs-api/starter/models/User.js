fc637d98aecab4b82af0305f159cdfb27068c578
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-19 18:20:34 -0500

    moving hashed pw to schema for cleaner code

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -22,6 +23,12 @@ const UserSchema = new mongoose.Schema({
         minlength:3,
         maxlength: 100,
     }
+})
+
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 
