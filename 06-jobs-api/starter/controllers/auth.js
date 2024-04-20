3274395993c8104a035e03cff3a8a96002b2850b
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-20 17:35:30 -0500

    Update auth.js

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -2,14 +2,14 @@ const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const {BadRequestError} = require('../errors/index')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 
 
 const register = async ( req, res) => {
-    
     const user = await User.create({...req.body})
+    const token = jwt.sign({userId:user._id, name:user.name}, process.env.JSON_WEB_TOKEN, {expiresIn:'30d'})
     console.log(user)
-    res.status(StatusCodes.CREATED).json({user})
+    res.status(StatusCodes.CREATED).json({user:{name:user.name}, token})
 }
 
 const login = async (req, res) => {
