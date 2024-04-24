36dc27ddae73ef4fadfb553875d98f82bbc92d99
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-24 00:06:11 -0500

    fixed env issue

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -6,8 +6,10 @@ const app = express();
 //connectDB
 const connectDB = require('./db/connect')
 
-// routers
+// protect routes with auth middleware
+const authenticateUser = require('./middleware/authentication');
 
+// routers
 const authRouter = require('./routes/auth')
 const jobRouter = require('./routes/jobs')
 
@@ -20,7 +22,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobRouter)
+app.use('/api/v1/jobs', authenticateUser, jobRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
