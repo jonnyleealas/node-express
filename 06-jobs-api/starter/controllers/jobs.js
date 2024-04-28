2afec3d382abd17b28e37372c7679cb8f31db7d7
Jonathon Lee <jonnyleealas@gmail.com>
2024-04-27 23:31:02 -0500

    added getJobs
    Will show jobs for a particular user ID

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -1,6 +1,11 @@
+const Job = require('../models/Job')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequesError, NotFoundError} = require('../errors/index')
 
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs')
+    const jobs = await Job.find({createdBy:req.user.userId}).sort('createAt')
+    res.status(StatusCodes.OK).json({jobs, count:jobs.length})
+    
 }
 
 const getJob = async (req, res) => {
@@ -8,7 +13,10 @@ const getJob = async (req, res) => {
 }
 
 const createJob = async (req, res) => {
-    res.json(req.body)
+    console.log(req.user, "request user")
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJob = async (req, res) => {
